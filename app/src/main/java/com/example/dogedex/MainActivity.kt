e28dccefd5185157b4c92145875615861fbf2453
Luisfayre <luisfayre@hotmail.com>
2022-08-02 19:24:11 -0600

    Implementaciòn de permiso de camara

--- app/src/main/java/com/example/dogedex/MainActivity.kt
+++ app/src/main/java/com/example/dogedex/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.dogedex
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import com.example.dogedex.api.ApiServiceInterceptor
 import com.example.dogedex.auth.LoginActivity
 import com.example.dogedex.databinding.ActivityMainBinding
@@ -11,6 +18,22 @@ import com.example.dogedex.models.User
 import com.example.dogedex.settings.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Open camera
+            } else {
+                Toast.makeText(
+                    this,
+                    "You need to accept camera permission to use camera",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         if (user == null) {
             openLoginActivity()
             return
-        } else{
+        } else {
             ApiServiceInterceptor.setSessionToken(user.authenticationToken)
         }
 
@@ -31,6 +54,42 @@ class MainActivity : AppCompatActivity() {
 
         binding.dogListFab.setOnClickListener {
             openDogListActivity()
+        }
+
+        requestCameraPermission()
+    }
+
+    private fun requestCameraPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // You can use the API that requires the permission.
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)
+                -> {
+                    AlertDialog.Builder(this)
+                        .setTitle("Aceptame por favor")
+                        .setMessage("Acepta la camara")
+                        .setPositiveButton(android.R.string.ok) { _, _ ->
+                            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+                        }
+                        .setNegativeButton(android.R.string.cancel) { _, _ ->
+                        }
+
+                }
+                else -> {
+                    // You can directly ask for the permission.
+                    // The registered ActivityResultCallback gets the result of this request.
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.CAMERA
+                    )
+                }
+            }
+        } else {
+            // Open camera
         }
     }
 
